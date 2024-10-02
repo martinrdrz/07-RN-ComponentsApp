683859177cf0bf96e81a5bf06b81bc400c03927a
martinrdrz <martinrdrz@hotmail.com>
2024-10-02 13:59:29 -0300

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';
+import {Text, View} from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
---
+++ src/presentation/icons/Icons.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+export const Icons = () => {
+  return (
+    <View>
+      <Text>Icons</Text>
+    </View>
+  );
+};
